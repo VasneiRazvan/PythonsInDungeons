399d8d6a07d015800eff477214f3a384f175c416
vasnei_razvan@live.com <Patratel91>
2021-06-03 19:55:41 +0300

    initial Player module commit

---
+++ player_module.py
@@ -0,0 +1,20 @@
+class Player:
+
+    def __init__(self, name, mana, health, damage):
+        self.name = name
+        self.mana = mana
+        self.health = health
+        self.damage = damage
+
+    def attack(self):
+        pass
+
+
+class Warrior(Player):
+    def __init__(self, name, mana, health, damage):
+        super.__init__(name, mana(0), health(10), damage(10))
+
+
+class Wizard(Player):
+    def __init__(self, name, mana, health, damage):
+        super.__init__(name, mana(100), health(10), damage(0))
