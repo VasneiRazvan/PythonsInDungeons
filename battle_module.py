5134150fe254ef1dc31c2a49ddbc836861240300
vasnei_razvan@live.com <Patratel91>
2021-06-17 19:00:05 +0300

    working sound on every stage commit

--- battle_module.py
+++ battle_module.py
@@ -1,5 +1,6 @@
 import utils_module
 import winsound
+import os
 battle_sound = "BattleFinal.wav"
 
 
@@ -7,5 +8,11 @@ def battle_enemy_village():
     winsound.PlaySound(battle_sound, winsound.SND_ASYNC)
     print("You are in the village ...")
     print("From a backside alley a enemy appears")
+    os.system("cls")
     utils_module.random_enemy()
-    input("what are you going to do? ...")
+    print("What are you going to do?")
+    input("""
+1. Fight the enemy
+2. Call for help
+3. Run for your life
+-> """)
